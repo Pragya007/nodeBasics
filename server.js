d1a836aba103ae2d27d9259bcc3c69fa4682e812
Pragya007 <pragsprincess24@gmail.com>
2020-03-23 23:49:11 +0530

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-var http = {}
+var http = require{'http'};
+function on Request(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);
